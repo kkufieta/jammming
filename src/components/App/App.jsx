cad0a65512e07879b60e45f3e0638fe2cf64ae44
kkufieta <katharina.kufieta@gmail.com>
2020-08-13 22:35:03 -0400

    Add basic structure of SearchBar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar.jsx';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
         {/* <!-- Add a SearchBar component --> */}
+        <SearchBar />
         <div className="App-playlist">
           {/* <!-- Add a SearchResults component --> */}
           {/* <!-- Add a Playlist component --> */}
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import './SearchBar.css'
+// Searchbar: take input, when user hits enter or clicks the button:
+// fetch the data from the spotify API
+// for now: console.log the data.
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    } g
+
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    handleKeyDown(event) {
+        if (event.key === 'Enter') {
+            // TODO: Submit the search query
+            console.log('key: ', event.key);
+            // TODO: Call Search
+            console.log("call search!")
+        }
+    }
+
+    handleClick(e) {
+        // TODO: Call Search
+        console.log("call search!")
+    }
+
     render() {
-        <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
-            <button className="SearchButton">SEARCH</button>
-        </div>
+        return (
+            <div className="SearchBar">
+                <input
+                    placeholder="Enter A Song, Album, or Artist"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    onKeyDown={this.handleKeyDown}
+                />
+                <button className="SearchButton" onClick={this.handleClick}>SEARCH</button>
+            </div>
+        )
     }
 }
 
