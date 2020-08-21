910a9db9f9d4d332f7621bba76dc2e2414f703f5
kkufieta <katharina.kufieta@gmail.com>
2020-08-21 12:05:31 -0400

    Change input value to defaultValue

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -5,7 +5,7 @@ class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
-                <input value="New Playlist" />
+                <input defaultValue={'New Playlist'} />
                 {/*<!-- Add a TrackList component -->*/}
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
