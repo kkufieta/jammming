42cdabbd2359ff8e10233ec66fc10ca79d46a66f
kkufieta <katharina.kufieta@gmail.com>
2020-08-23 17:47:26 -0400

    Updates

---
+++ precache-manifest.94bcde50ce06666ef6a9860851e714a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47db62eb7ff2e72074ce241f3e5ddc82",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "64aeb2b75dba8f7d5979",
+    "url": "/jammming/static/css/main.f9b87a18.chunk.css"
+  },
+  {
+    "revision": "7cbc595d2f4a828d2a34",
+    "url": "/jammming/static/js/2.72f751fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jammming/static/js/2.72f751fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64aeb2b75dba8f7d5979",
+    "url": "/jammming/static/js/main.b12cc25a.chunk.js"
+  },
+  {
+    "revision": "d54c33d011ceab8fe7ea",
+    "url": "/jammming/static/js/runtime-main.af6152f4.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
