4a1f04758b8dac2b0ee68817dc41866e1bc9bf8c
kkufieta <katharina.kufieta@gmail.com>
2020-08-26 19:57:32 -0400

    Add Github pages as a callback uri

--- src/util/config.js
+++ src/util/config.js
@@ -1,3 +1,3 @@
-const spotifyClientId = 'b5ea5928b1984e0e9b1fd6c8048fb164';
-
-export default spotifyClientId;
+export const spotifyClientId = 'b5ea5928b1984e0e9b1fd6c8048fb164';
+// export const redirectUri = 'http://localhost:3000/';
+export const redirectUri = 'https://kkufieta.github.io/jammming/';
